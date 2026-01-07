dee52c902e0e264ba6d4a3512d23294d4edefd3f
mohamedhamido <76883197+mohamedhamido@users.noreply.github.com>
2026-01-07 03:08:44 +0300

    dd production build in docs for itub ages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  plugins: [react()],
+
+  // مهم جداً حتى يعمل الموقع على GitHub Pages تحت المسار الفرعي
   base: '/hamido-game-studio/',
-  plugins: [react()]
+
+  // نجعل مخرجات البناء في مجلد docs حتى تستخدمه GitHub Pages مباشرة
+  build: {
+    outDir: 'docs',
+  },
 });
